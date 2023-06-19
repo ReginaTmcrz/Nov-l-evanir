8f6ad86ce506167a26838a0ca4e21a39c54111f8
Regina Tomacruz <104839870+ReginaTmcrz@users.noreply.github.com>
2023-06-19 20:42:37 +0800

    Add files via upload

---
+++ model/Seraphina.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Seraphina extends Product {
+
+    public void backBtnClick(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/cataloguePage.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    
+}
